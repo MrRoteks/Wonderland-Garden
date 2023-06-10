299806be4d19ce1d3d3c589256d627420c75810d
MrRoteks <rotkol@wp.pl>
2023-06-10 19:25:42 +0200

    Added Final Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Wonderland-Garden
-Web App project presenting a website for garden design company. Project for ZTPAI classes.
---
+++ wonderlandgarden/frontend/src/components/TestPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+export default class TestPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+<p>This is the TestPage</p>
+    );
+  }
+}
